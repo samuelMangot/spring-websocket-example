3c14f3f81245afd30a19dad3272a650314cbaeb5
nerdyprog <nerdyprog@gmail.com>
2014-06-18 17:17:07 +0200

    * APP

---
+++ spring-websocket-example-server/src/main/webapp/app/libs/sockjs-client-master/lib/trans-iframe-eventsource.js
@@ -0,0 +1,27 @@
+'use strict';
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Copyright (c) 2011-2012 VMware, Inc.
+ *
+ * For the license see COPYING.
+ * ***** END LICENSE BLOCK *****
+ */
+
+var IframeTransport = require('./trans-iframe');
+
+function EventSourceIframeTransport() {
+    var that = this;
+    that.protocol = 'w-iframe-eventsource';
+    that.i_constructor.apply(that, arguments);
+}
+
+EventSourceIframeTransport.prototype = new IframeTransport();
+
+EventSourceIframeTransport.enabled = function () {
+    return ('EventSource' in window) && IframeTransport.enabled();
+};
+
+EventSourceIframeTransport.need_body = true;
+EventSourceIframeTransport.roundTrips = 3; // html, javascript, eventsource
+
+module.exports = EventSourceIframeTransport;
